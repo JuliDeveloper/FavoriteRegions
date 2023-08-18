31efae8b246faa3a7752774981a0164f131e8d86
JuliaRomanenko <rja-97@yandex.ru>
2023-08-19 00:33:31 +0300

    Вынесла настройку внешнего вида кнопки в отдельный класс

---
+++ FavoriteRegions/CustomViews/LikeButton.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class LikeButton: UIButton {
+    
+    init() {
+        super.init(frame: .zero)
+        setImage(UIImage(named: "noActiveIsLike"), for: .normal)
+        tintColor = .frGrayColor
+        contentMode = .scaleAspectFit
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
